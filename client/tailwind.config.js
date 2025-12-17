275bd5d64abce359c1e12b44868e876bc88fbad0
KeerthiKumar2902 <keerthikumarkk1@gmail.com>
2025-12-17 21:32:14 +0530

    Installed tailwind css modules

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          50: '#f0fdf4', // Very light green (backgrounds)
+          100: '#dcfce7', // Light green (hover states)
+          500: '#22c55e', // Primary Green (buttons, highlights)
+          600: '#16a34a', // Darker Green (text)
+          900: '#14532d', // Deep Green (headings)
+        }
+      }
+    },
   },
   plugins: [],
 }
